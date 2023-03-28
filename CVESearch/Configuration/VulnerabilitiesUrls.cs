b6579d3f060dcf1351d7b6f8ed2c64030a931c84
Bohdan Nalyvaiko <b.nalyvaiko@apptimized.com>
2023-03-28 10:37:54 +0300

    add Urls config

---
+++ CVESearch/Configuration/VulnerabilitiesUrls.cs
@@ -0,0 +1,9 @@
+﻿namespace CVESearch.Configuration
+{
+    public class VulnerabilitiesUrls
+    {
+        public string CveUrl { get; set; }
+        public string CweUrl { get; set; }
+        public string CapecUrl { get; set; }
+    }
+}
